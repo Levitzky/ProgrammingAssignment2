3f16601f857173f85ad34bd44f1a0c07f034ba86
levitzky <michaltzahi@yahoo.com>
2014-12-20 23:17:50 -0500

    sync R file for course assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,9 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## These are a pair of functions that cache the inverse of a matrix x. 
+## However, it first checks to see if the mean has already been calculated. If so, it gets the mean from the cache and skips the computation. Otherwise, it calculates the mean of the data and sets the value of the mean in the cache via the setmean function.
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
